34bb85aad312a3d45f787eab30605a6d888b981c
AmmrWahab <ammrwhab@gmail.com>
2025-07-26 23:14:58 +0500

    Force rebuild: update comment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,3 +5,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// Rebuild trigger – July 25, 2025 – Ammr Wahab
